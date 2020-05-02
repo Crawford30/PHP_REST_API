7cf146873371a19d611c02c6f498dc6e0a055641
joel <joelcrawford30@gmail.com>
2020-05-02 13:24:29 +0300

    The create method, the bind_param and execute method

---
+++ classes/student.php
@@ -0,0 +1,107 @@
+<?php 
+
+class Student {
+
+	//we declare some varaibles, making sure they correspond to the database details
+
+	//we declared them public because we gonna access them in the create.php and databse.php
+
+	 public $name;
+	 public $email;
+     public $mobile;
+
+     //connection varaible and table name varaibele
+
+     //mean while these two below are going tpo be internally used in this class.
+     private $conn;
+     private $table_name;
+
+
+     //Define our condtrct function, ie construtor
+
+     public function  _construct($db) {
+     	//$db is the connect object variable
+
+
+     	//initalisation
+
+     	$this ->  conn = $db ; //will start the connection variable
+
+
+     	$this -> table_name = "tbl_students";  //our table name is tbl_students
+
+
+
+     }
+
+     //Method for cresating data in the database tabele
+
+     public function create_data() {
+
+     	//we write the insert query, sql query to insert data
+
+     	$query = "INSERT INTO ".$this -> table_name." 
+     	SET name = ?,  email = ?, mobile = ?";
+
+     	//prepare the sql query
+     	$obj = $this -> conn -> prepare($query);   //use prepare method and pass our query variable
+
+
+     	//Before inserting , we need to sanitize the values, name, email, mobile. sanitizs we remove some special charaters
+
+     	//sanitize input variables => removes the extra xters like some special symbols as well as if son=me tags avalioable in input values
+
+     	//we use 2 php functions, one is html special character, and history tags
+
+
+     	$this -> name = htmlspecialchars(strip_tags($this -> name)); 
+
+     	$this -> email = htmlspecialchars(strip_tags($this -> email)); 
+
+     	$this -> mobile = htmlspecialchars(strip_tags($this -> mobile)); 
+
+
+
+     //We now bind the parameters to the placeholders,  name = ?,  email = ?, mobile = ?
+     //ie $this -> name to  name = ?
+
+
+     	$obj -> bind_param("SSS", $this -> name, $this -> email, $this -> mobile); //emthod used to bind parameters  to the place holders, the sss means the name as a string  valeu, email as a string value and mobile as a string value. if it has interger we use i
+
+
+     	//WE need to execute, we check the obj
+
+     	if ($obj -> execute()) { //executin the query
+
+
+     		return true;
+
+
+
+     	}
+
+     	return false;
+
+
+
+
+
+
+     }
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+?>
